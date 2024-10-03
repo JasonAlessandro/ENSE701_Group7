45727af8493aa68fe277b0ebaaf921d72010df29
ErickLao123 <105694432+ErickLao123@users.noreply.github.com>
2024-10-03 21:06:49 +1300

    Delete _app.tsx

--- _app.tsx
+++
@@ -1,7 +0,0 @@
-// src/pages/_app.tsx
-import { AppProps } from 'next/app';
-import '../styles/globals.css';
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
