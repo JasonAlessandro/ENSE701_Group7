01f872cea1e474dc6d22f565f66f4737c58e4bf6
JasonAlessandro <105477409+JasonAlessandro@users.noreply.github.com>
2024-10-18 23:37:09 +1300

    moderation removed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);// enable cors
+    const app = await NestFactory.create(AppModule);
     app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS' });
     const port = process.env.PORT || 8082;
     await app.listen(port, () => console.log('Server running on port ${port}'));
